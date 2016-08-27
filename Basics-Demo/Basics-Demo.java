162ad90016b94e576b57cc507ab2bdabdc8850d8
Aralya Phinith <aphinith84@gmail.com>
2016-08-27 01:39:30 -0700

    practice with syntax and cli using java

--- Basics-Demo/Basics-Demo.java
+++ Basics-Demo/Basics-Demo.java
@@ -1,17 +1,37 @@
 class BasicsDemo {
-  // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
   static void print() {
     System.out.println("\n\nInside print ...");
-    System.out.println("Hello, world!!"); // Advance cursor to beginning of next line
-    System.out.println();                 // Print empty line
-    System.out.print("Hello, world!!");   // Cursor stayed after the printed string
-    System.out.println("Hello,");
-    System.out.print(" ");                // Print a space
-    System.out.print("world!!");
+    System.out.println("Hello, world!!!");
+    System.out.println();
+    System.out.print("Hello, world!!!");
+    System.out.print(" ");
+    System.out.print("Hello,");
+    System.out.print(" ");
+    System.out.print("World!!!");
+    System.out.println();
   }
-  
-  public static void main(String[] args) {	
-	   	// Language Basics 1
-		print();				
-  }       
+
+  public static void main(String[] args) {
+    print();
+  }
 }
+
+
+
+// class BasicsDemo {
+//   // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
+//   static void print() {
+//     System.out.println("\n\nInside print ...");
+//     System.out.println("Hello, world!!"); // Advance cursor to beginning of next line
+//     System.out.println();                 // Print empty line
+//     System.out.print("Hello, world!!");   // Cursor stayed after the printed string
+//     System.out.println("Hello,");
+//     System.out.print(" ");                // Print a space
+//     System.out.print("world!!");
+//   }
+  
+//   public static void main(String[] args) {	
+// 	   	// Language Basics 1
+// 		print();				
+//   }       
+// }
