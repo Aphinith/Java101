46d872434f9f7689ea3439f1717e74b8b508747e
Aralya Phinith <aphinith84@gmail.com>
2016-08-27 01:01:22 -0700

    create two classes, compile, and execute

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
-class {
+class HelloWorld {
   public static void main (String[] args) {
-    System.out.println("HelloWorld!!");
+    System.out.println("HelloWorld!!!");
+  }
+}
+
+class GoodBye {
+  public static void main (String[] args) {
+    System.out.println("GoodBye!!!");
   }
 }
