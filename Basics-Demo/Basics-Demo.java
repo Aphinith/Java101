a0369f86c0bcce514771985a239be53c1d3dab7d
Aralya Phinith <aphinith84@gmail.com>
2016-08-31 23:37:27 -0700

    practice with typeCasting

--- Basics-Demo/Basics-Demo.java
+++ Basics-Demo/Basics-Demo.java
@@ -1,4 +1,5 @@
 class BasicsDemo {
+
   static void print() {
     System.out.println("\n\nInside print ...");
     System.out.println("Hello, world!!!");
@@ -19,11 +20,53 @@ class BasicsDemo {
     System.out.println("intBinary: " + intBinary);
     int intUnderscore = 1_23_456;
     System.out.println("intUnderscore: " + intUnderscore);
+
+    char charA = 'A';
+    System.out.println("charA: " + charA);
+    char charInt = 65;
+    System.out.println("charInt: " + charInt);
+    char charUnicode1 = '\u0041';
+    System.out.println("charUnicode1: " + charUnicode1);
+    char charUnicode2 = 0x41;
+    System.out.println("charUnicode2: " + charUnicode2);
+    char charBinary = 0b01000001;
+    System.out.println("charBinary: " + charBinary);
+
+  }
+
+  static void typeCasting() {
+    System.out.println("\nInside typeCasting...");
+    // Explicit casting;
+    long y = 42;
+    // int x = y;
+    int x = (int)y;
+
+    // information loss due to out-of-range assignment
+    byte narrowdByte = (byte)123456;
+    System.out.println("narrowdByte: " + narrowdByte);
+
+    // Truncation
+    int iTruncated = (int)0.99;
+    System.out.println("iTruncated: " + iTruncated);
+
+    // Implicit cast (int to long)
+    y = x;
+
+    //Implicit cast (char to int)
+    char cChar = 'A';
+    int iInt = cChar;
+    System.out.println("iInt: " + iInt);
+
+    // byte to char using an explicit cast
+    byte bByte = 65;
+    cChar = (char)bByte; // special conversion (widening from byte --> int followed by narrowing from int -- > char)
+    System.out.println("cChar: " + cChar);
   }
 
   public static void main(String[] args) {
     // print();
-    primitives();
+    // primitives();
+    typeCasting();
   }
 }
 
